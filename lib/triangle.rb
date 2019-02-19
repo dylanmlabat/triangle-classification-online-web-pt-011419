2eff8102442917a910c8f3845ea670f189525673
Dylan Michael Labat <dml252@live.com>
2019-02-19 22:39:58 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,6 +13,9 @@ class Triangle
     elsif side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1
       raise TriangleError
     else
+      if side1 == side2 && side1 == side3 && side2 == side3
+        :equilateral
+      elsif side1
     end
   end
   
