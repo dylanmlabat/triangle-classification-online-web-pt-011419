721920612e4dd956227814c3208f8e3f1bd0352e
Dylan Michael Labat <dml252@live.com>
2019-02-19 22:40:29 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,9 @@ class Triangle
     else
       if side1 == side2 && side1 == side3 && side2 == side3
         :equilateral
-      elsif side1
+      elsif side1 == side2 || side1 == side3 || side2 == side3
+        :isosceles
+      elsif
     end
   end
   
